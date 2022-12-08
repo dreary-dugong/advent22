38c46f4ec9a0ed4713c0af39f5f2afefc89c1a27
Daniel Gysi <danielgysi@protonmail.com>
2022-12-07 23:19:58 -0500

    allegedly improved day 7 part 1 in python (the algorithm is much smarter but the runtime is the same because python)

--- day7/1/python/solution.py
+++ day7/1/python/solution.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 # solution for https://adventofcode.com/2022/day/7 part 1
 
-# constants
-INPUT_FILE = "../input.txt"
+# imports
+import timeit
+from collections import deque
 
 # helper classes
 class Directory:
+    """represent a directory that can contain other files in a filesystem"""
+
     def __init__(self, name, parent):
         self.name = name
         self.contents = []
@@ -18,43 +21,47 @@ class Directory:
                 return item
         return None
 
-    def get_size(self):
-        """return the total size of all contents of the directory"""
-        # this current implementation means a lot of rework in the script later on
-        # this hasn't been a problem because the input is small but it could be fixed if needed
-        size = 0
-        for item in self.contents:
-            if type(item) == Directory:
-                size += item.get_size()
-            elif type(item) == File_:
-                size += item.size
-        self.size = size
-        return size
-
     def get_complete_path(self):
         """return a string of the complete path from root"""
-        if not parent:
+        if not self.parent:
             return self.name
         else:
             return self.parent.get_complete_path() + self.name + "/"
 
     def __hash__(self):
-        return self.get_complete_path.__hash__()
+        """magic method for getting a hash of the object"""
+        return self.get_complete_path().__hash__()
 
 
 class File_:
+    """represent a file in a filesystem (not a directory)"""
+
     def __init__(self, name, size):
         self.name = name
         self.size = size
 
 
-def main():
-    # read input
-    with open(INPUT_FILE) as f:
+class StackFrame:
+    """represent an item in our queue for the size finding algorithm"""
+
+    def __init__(self, item):
+        self.item = item
+        self.contents_ptr = 0
+        self.size = 0
+
+
+# helper functions
+def read_input(input_file):
+    """read input from file and convert it to lines of commands and output"""
+    with open(input_file) as f:
         data = f.read().strip()
     lines = data.split("\n")
+    return lines
 
-    # parse commands and output to make file system
+
+def parse_lines(lines):
+    """given lines of commands and output, construct a file system and list of directories"""
+
     wd = Directory("/", None)  # we assume the first line  is "$ cd /"
     directories = []  # maintain a running list of all directories
     line_pointer = 1
@@ -77,30 +84,104 @@ def main():
 
             # get containing files from output
             line_pointer += 1
+            # read in lines as files until we reach the end of the input or the next command
             while line_pointer < len(lines) and lines[line_pointer][0] != "$":
                 cur_line = lines[line_pointer]
                 tokens = cur_line.split(" ")
 
-                # is the file a directory or a file?
+                # is the item a directory or a file?
                 if tokens[0] == "dir":
-                    item = Directory(tokens[1], wd)
+                    name = tokens[1]
+                    parent = wd
+                    item = Directory(name, parent)
                     directories.append(item)
                 elif tokens[0].isnumeric():
-                    item = File_(tokens[1], int(tokens[0]))
-                else:
-                    print(f"Attempt to create file from {cur_line}")
+                    size = int(tokens[0])
+                    name = tokens[1]
+                    item = File_(name, size)
+
                 wd.contents.append(item)
 
                 line_pointer += 1
 
-    # with the file system fully constructed, we get the sizes of all directories
-    total_under_100k = 0  # the total size of all directories whose size is under 100k
-    for directory in directories:
-        size = directory.get_size()
-        if size < 100000:
-            total_under_100k += size
+    return directories
 
-    print(total_under_100k)
+
+def total_size_under_n(directories, n):
+    """given a list of directories, return the total size of all directories under n in size"""
+    total_size = 0  # total size of directories under 100k each, our output
+    memo = dict()  # dictionary of directories with known sizes
+    queue = deque(
+        map(StackFrame, directories)
+    )  # queue of directories whose size we don't know yet
+
+    # while there are still directories with unknown sizes
+    while queue:
+        frame = queue.pop()
+        size_known = True  # flag to add frame back to the queue later if needed
+        cur_dir, contents_ptr, cur_size = frame.item, frame.contents_ptr, frame.size
+        # calculate size by going over items in the directory
+        for i, item in enumerate(cur_dir.contents[contents_ptr:]):
+
+            # if the item is a file, just add the size
+            if type(item) == File_:
+                cur_size += item.size
+                if cur_size > n:  # if at any point the size goes over n, stop counting
+                    cur_size = None
+                    break
+
+            # if the item is a directory and we know its size, just add it
+            elif (type(item) == Directory) and (item in memo):
+                subdir_size = memo[item]
+                if (subdir_size is None) or ((cur_size + subdir_size) > n):
+                    cur_size = None
+                    break
+                else:
+                    cur_size += subdir_size
+
+            # otherwise, we'll need to get the size of the new directory first
+            else:
+                size_known = False
+                break
+
+        # if we've gone through all the items and know the size, add it to memo
+        if size_known:
+            memo[cur_dir] = cur_size
+            if cur_size is not None:
+                total_size += cur_size
+
+        # otherwise, stick it at the end of the queue to resume later
+        else:
+            frame.contents_ptr += i
+            frame.size = cur_size
+            queue.appendleft(frame)
+
+    return total_size
+
+
+def solve():
+    """return the solution to the challenge"""
+    # mostly this function just exists to make benchmarking easier
+    # constants
+    INPUT_FILE = "../input.txt"
+    N = 100000
+
+    lines = read_input(INPUT_FILE)
+    directories = parse_lines(lines)
+    total_size_under_100k = total_size_under_n(directories, N)
+
+    return total_size_under_100k
+
+
+def benchmark():
+    """run the solution 10k times and output the number of seconds that took"""
+    setup_code = "from __main__ import solve"
+    test_code = "solve()"
+    print(timeit.timeit(setup=setup_code, stmt=test_code, number=10000))
+
+
+def main():
+    print(solve())
 
 
 if __name__ == "__main__":
